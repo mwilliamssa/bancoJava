5f3c031c2d21c3a28e3e231864ab0a794ef7f567
Matheus Williams <matheuswilliamsdev@gmail.com>
2025-05-08 18:05:08 -0300

    Mostre a informacoes do cliente no sistema

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Dados do cliente:
         String nome = "Matheus Williams";
         String tipoConta = "Corrente";
         double saldoInicial = 2500;
         double pix;
         boolean saldoInsuficiente = false;
-        // Menu:
         Scanner leitura = new Scanner(System.in);
-        boolean parar = false;
         int opcoes = 0;
-
-        while (opcoes != 4) {
+        System.out.printf(String.format("""
+                Dados do cliente:
+                
+                Nome do cliente: %s
+                Tipo de conta do cliente: %s
+                Saldo do cliente: R$%.2f
+                
+                
+                """, nome, tipoConta, saldoInicial));
+        while (opcoes != 4){
             System.out.println("""
                     Operações
                     
@@ -28,22 +33,22 @@ public class Main {
             switch (opcoes) {
                 case 1:
                     System.out.println("Consultar sados");
-                    System.out.println(String.format("O saldo é de R$%.2f", saldoInicial));
+                    System.out.printf("O saldo é de R$%.2f%n", saldoInicial);
                     break;
                 case 2:
                     System.out.println("Receber valor");
                     pix = leitura.nextDouble();
                     saldoInicial += pix;
-                    System.out.println(String.format("Seu saldo atual é de R$%.2f", saldoInicial));
+                    System.out.printf("Seu saldo atual é de R$%.2f%n", saldoInicial);
                     break;
                 case 3:
                     System.out.println("Sacar valor");
-                    while(saldoInsuficiente != true) {
+                    while(!saldoInsuficiente) {
                         System.out.println("Qual o valor desejado para saque?");
                         pix = leitura.nextDouble();
                         if (pix > saldoInicial){
                             System.out.println("Saldo insuficiente");
-                            System.out.println(String.format("Seu saldo é de R$%.2f", saldoInicial));
+                            System.out.printf("Seu saldo é de R$%.2f%n", saldoInicial);
                             System.out.println("Deseja tentar novamente? Digite 1 para sim e 2 para não:");
                             int escolha = leitura.nextInt();
                             switch (escolha){
@@ -55,14 +60,13 @@ public class Main {
                             }
                         } else {
                             saldoInicial -= pix;
-                            System.out.println(String.format("Seu saldo atual é de R$%.2f", saldoInicial));
+                            System.out.printf("Seu saldo atual é de R$%.2f%n", saldoInicial);
                             saldoInsuficiente = true;
                         }
                     }
                     break;
                 case 4:
                     System.out.println("Sair");
-                    opcoes = 4;
                     break;
                 default:
                     System.out.println("Erro");
